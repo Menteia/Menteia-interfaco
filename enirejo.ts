f9f2b4edfcde08a8abe4c794ab0f0a024d222f38
Sam Wang <samofcorinth@gmx.ca>
2019-01-21 01:18:16 -0500

    Finis aldonadon

--- enirejo.ts
+++ enirejo.ts
@@ -5,66 +5,39 @@ import * as readline from "readline";
 import * as inquirer from "inquirer";
 import moment from "moment";
 
-import { legiDosieron, konserviDosieron } from "./vortaro";
-import { Legilo } from "./legilo";
-import { Kontrolilo } from "./kontrolilo";
-import { paroli } from "./polly";
-import { respondi } from "./cerbo";
+import { aldoniVorton, transferi } from "./vortaro";
 
 interface Respondo {
   eniro: string;
 }
 
-legiDosieron().then(vortaro => {
-  const kontrolilo = new Kontrolilo(vortaro);
-  const legilo = new Legilo(vortaro);
-
-  const eniri = () => {
-    inquirer
-      .prompt<Respondo>([
-        {
-          name: "eniro",
-          message: ">"
-        }
-      ])
-      .then(({ eniro }) => {
-        const partoj = eniro.split(" ");
-        switch (partoj[0]) {
-          case "aldoni":
-            const vorto = partoj[1];
-            if (vorto == null) {
-              console.log("Nevalida aldono");
-              break;
-            }
-            if (vortaro.has(vorto)) {
-              console.log(`La vorto ${vorto} jam ekzistas.`);
-            } else {
-              if (kontrolilo.validas(vorto)) {
-                vortaro.set(vorto, [partoj[2], parseInt(partoj[3])]);
-                konserviDosieron(vortaro);
-              } else {
-                console.log(`${vorto} ne estas valida.`);
-              }
-            }
-            break;
-          case "eliri":
-            return;
-          case "paroli":
-            const arbo = legilo.kompreni(partoj.slice(1).join(""));
-            paroli(arbo, partoj.slice(1).join(" ") + ".ogg");
-            break;
-          default:
-            try {
-              respondi(legilo.kompreni(eniro)).then((respondo) => {
-                paroli(respondo, moment().format('YYYY-MM-DD HHmmss') + ".ogg");
-              });
-            } catch (e) {
-              console.error(e);
-            }
-            break;
-        }
-        eniri();
-      });
-  };
-  eniri();
-});
+const eniri = () => {
+  inquirer
+    .prompt<Respondo>([
+      {
+        name: "eniro",
+        message: ">"
+      }
+    ])
+    .then(({ eniro }) => {
+      const partoj = eniro.split(" ");
+      switch (partoj[0]) {
+        case "aldoni":
+          const vorto = partoj[1];
+          aldoniVorton(vorto, partoj[2]).catch((e) => {
+            console.error(e);
+          }).then((data) => {
+            console.log(data);
+          }).finally(() => {
+            eniri();
+          });
+          break;
+        case "eliri":
+          return;
+        default:
+          eniri();
+          break;
+      }
+    });
+};
+eniri();
--- vortaro/index.ts
+++ vortaro/index.ts
@@ -2,32 +2,64 @@ import * as AWS from "aws-sdk";
 import * as fs from "fs";
 import * as readline from "readline";
 
-const vortaroDosiero = "vortaro.txt";
-let legitaVortaro: Map<string, [string, number]> | undefined;
+const db = new AWS.DynamoDB({region: 'us-west-2'});
+const lambda = new AWS.Lambda({region: 'us-east-1'});
+const tabeloNomo = "Menteia-vortaro";
 
-export async function legiDosieron(): Promise<Map<string, [string, number]>> {
-  if (legitaVortaro) return legitaVortaro;
-  const rl = readline.createInterface(fs.createReadStream(vortaroDosiero));
-  const vortaroPostaĵo = new Promise<Map<string, [string, number]>>(
-    (resolve, reject) => {
-      const vortaro = new Map<string, [string, number]>();
-      rl.on("line", vico => {
-        const [m, e, n] = vico.split("/");
-        vortaro.set(m, [e, parseInt(n)]);
-      });
-      rl.on("close", () => {
-        legitaVortaro = vortaro;
-        resolve(legitaVortaro);
-      });
-    }
-  );
-  return vortaroPostaĵo;
-}
-
-export function konserviDosieron(vortaro: Map<string, [string, number]>) {
-  const dosiero = fs.createWriteStream(vortaroDosiero);
-  for (const [m, [e, n]] of vortaro) {
-    dosiero.write(`${m}/${e}/${n}\n`);
-  }
-  dosiero.close();
+export async function aldoniVorton(vorto: string, valenco: string): Promise<AWS.DynamoDB.PutItemOutput> {
+  return new Promise((akcepti, malakcepti) => {
+    lambda.invoke({
+      FunctionName: 'Menteia-Kontrolilo',
+      Payload: JSON.stringify({
+        Vorto: vorto
+      })
+    }, (err, data) => {
+      if (err) {
+        malakcepti(err);
+      } else if (typeof data.Payload === "string") {
+        const respondo: {ĈuValida: boolean, Kialo: string} = JSON.parse(data.Payload);
+          db.scan({
+          TableName: tabeloNomo
+        }, (err, data) => {
+          if (err) {
+            malakcepti(err);
+          } else if (data.Items == null) {
+            malakcepti(new Error(`Ne eblas legi la vortaron`));
+          } else {
+            for (const v of data.Items) {
+              const ekzistantaVorto = v.vorto.S;
+              if (ekzistantaVorto != null &&
+                  (vorto.startsWith(ekzistantaVorto) ||
+                  ekzistantaVorto.startsWith(vorto) ||
+                  vorto.endsWith(ekzistantaVorto) ||
+                  ekzistantaVorto.endsWith(vorto))) {
+                malakcepti(new Error(`La vorto ${vorto} trafas kun ${ekzistantaVorto}`));
+                return;
+              }
+            }
+            db.putItem({
+              TableName: tabeloNomo,
+              Item: {
+                vorto: {
+                  S: vorto
+                },
+                valenco: {
+                  N: valenco
+                },
+              },
+              ConditionExpression: "attribute_not_exists(Vorto)"
+            }, (err, data) => {
+              if (err) {
+                malakcepti(err);
+              } else {
+                akcepti(data);
+              }
+            });
+          }
+        });
+      } else {
+        malakcepti(new Error("Ne eblas kontroli la vorton"));
+      }
+    })
+  });
 }
